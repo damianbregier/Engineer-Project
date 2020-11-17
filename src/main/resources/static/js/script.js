4245e92e0594371c71b0bb6529ed1dabcf977578
Damian Bregier <damian.bregier@gmail.com>
2020-11-17 20:51:50 +0100

    ADD: Loader

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,10 @@
+$(window).on("load", function () {
+  
+    /* Loader control */
+    $(".loader .loader-inner").fadeOut(3000, function () {
+      $(".loader").fadeOut(1000);
+    });
+  
+  });
+  
+ 
