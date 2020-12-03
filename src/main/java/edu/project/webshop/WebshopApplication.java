d457f7e4c4ca0f35d2145772c410ffeb464f049d
Damian Bregier <damian.bregier@gmail.com>
2020-12-03 21:10:12 +0100

    IMPROVE: Shop layout

--- src/main/java/edu/project/webshop/WebshopApplication.java
+++ src/main/java/edu/project/webshop/WebshopApplication.java
@@ -1,4 +1,4 @@
-package com.project.webshop;
+package edu.project.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
