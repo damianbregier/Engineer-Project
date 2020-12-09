508d52b80ba6eec23453b91b82bd35fc458e7a88
Damian Bregier <damian.bregier@gmail.com>
2020-12-09 20:05:12 +0100

    ADD: User service

---
+++ src/main/java/edu/project/webshop/service/UserService.java
@@ -0,0 +1,58 @@
+package edu.project.webshop.service;
+
+import edu.project.webshop.repository.UserRepository;
+import edu.project.webshop.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.project.webshop.repository.UserRepository;
+import edu.project.webshop.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //save ONE user
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    //allow to save MULTIPLE users
+    public List<User> saveUsers(List <User> users){
+        return userRepository.saveAll(users);
+    }
+
+    //return one particular user based on id
+    public User getUserById(int id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    //return all existing users
+    public List <User>  getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public String deleteUser(int id){
+        userRepository.deleteById(id);
+        return "User was successfully removed!";
+    }
+
+    public User updateUser(User user){
+        User existingUser = userRepository.findById(user.getId()).orElse(null);
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+        return  userRepository.save(existingUser);
+    }
+
+
+}
