f60239c0de6f94194be343ab4141e3822b3dca45
Damian Bregier <damian.bregier@gmail.com>
2021-01-05 12:35:21 +0100

    IMPROVE: Exclude admin form user list in usersView

--- src/main/java/edu/project/webshop/controller/UserController.java
+++ src/main/java/edu/project/webshop/controller/UserController.java
@@ -1,5 +1,6 @@
 package edu.project.webshop.controller;
 
+import edu.project.webshop.entity.Role;
 import edu.project.webshop.entity.User;
 import edu.project.webshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.HashSet;
 import java.util.List;
 
 @Controller
@@ -46,6 +48,19 @@ public class UserController {
 
     @GetMapping("/usersView")
     public String viewHomePage(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User loggedUser = userService.findUserByEmail(auth.getName());
+        HashSet<Role> loggedUserRoles = new HashSet<>(loggedUser.getRoles());
+        String log_user_role = "";
+        for(Role role : loggedUserRoles){
+            if(role.getRole().equals("ADMIN")){
+                log_user_role = String.valueOf(role.getRole());
+            }else if(role.getRole().equals("USER")){
+                log_user_role = String.valueOf(role.getRole());
+            }
+        }
+        model.addAttribute("log_user_role", log_user_role);
+
         return findPaginated(1,"email", "asc", model);
     }
 
