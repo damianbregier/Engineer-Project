a653ef8c0ed12af082a83635e19a8ec2a859f21a
Damian Bregier <damian.bregier@gmail.com>
2021-01-09 18:02:44 +0100

    ADD: Tag and Course list to shop controller

--- src/main/java/edu/project/webshop/controller/ShopController.java
+++ src/main/java/edu/project/webshop/controller/ShopController.java
@@ -1,7 +1,11 @@
 package edu.project.webshop.controller;
 
+import edu.project.webshop.entity.Course;
 import edu.project.webshop.entity.Role;
+import edu.project.webshop.entity.Tag;
 import edu.project.webshop.entity.User;
+import edu.project.webshop.service.CourseService;
+import edu.project.webshop.service.TagService;
 import edu.project.webshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashSet;
+import java.util.List;
 
 @Controller
 public class ShopController {
@@ -19,11 +24,24 @@ public class ShopController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    TagService tagService;
+
+    @Autowired
+    CourseService courseService;
+
     @GetMapping(value = "/shop")
     public String shop(Model model){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User loggedUser = userService.findUserByEmail(auth.getName());
         HashSet<Role> loggedUserRoles = new HashSet<>(loggedUser.getRoles());
+
+        List<Tag> listTags = tagService.getAllTags();
+        model.addAttribute("listTags", listTags);
+
+        List<Course> listCourse = courseService.getAllCourses();
+        model.addAttribute("listCourse", listCourse);
+
         String log_user_role = "";
         for(Role role : loggedUserRoles){
             if(role.getRole().equals("ADMIN")){
