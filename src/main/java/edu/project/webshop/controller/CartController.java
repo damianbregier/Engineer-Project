f9bc42e40f233a194e8313ff62d50c8c30e0b251
Damian Bregier <damian.bregier@gmail.com>
2021-01-19 20:03:52 +0100

    FIX: Split cart mapping into post and get

--- src/main/java/edu/project/webshop/controller/CartController.java
+++ src/main/java/edu/project/webshop/controller/CartController.java
@@ -11,9 +11,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashSet;
 import java.util.List;
 
 @Controller
@@ -31,8 +31,34 @@ public class CartController {
     @Autowired
     CartItemService cartItemService;
 
-    @GetMapping("/addToCart/{id}")
-    public String addToCart(@PathVariable(value = "id") int id, Model model){
+    /*
+    To make it work properly post mapping should take care of assigning things
+    and then redirect to "/cart" which is get mapping name
+    while get mapping takes care of showing list of items
+
+    using post mapping to show list of items always doesn't show latest item
+    while refreashing page adds more items to cart!
+     */
+
+    @GetMapping("/cart")
+    public String cart(Model model){
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User loggedUser = userService.findUserByEmail(auth.getName());
+
+        int curentId = loggedUser.getId();
+        Cart cart = cartService.findCartByid(loggedUser.getId());
+        List<CartItem> cartItemList = cartItemService.findAllCartItems();
+
+        model.addAttribute("cartItemList", cartItemList);
+        model.addAttribute("currentId", curentId);
+        return "cart";
+    }
+
+
+
+    @PostMapping("/cart")
+    public String addToCart(@RequestParam int id, Model model){
         Course course = courseService.getCourseById(id);
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -41,13 +67,6 @@ public class CartController {
         int curentId = loggedUser.getId();
 
         Cart cart = cartService.findCartByid(loggedUser.getId());
-
-        List<CartItem> cartItemList = cartItemService.findAllCartItems();
-
-
-        model.addAttribute("cartItemList", cartItemList);
-        model.addAttribute("currentId", curentId);
-
         CartItem cartItem = new CartItem();
         cartItem.setCourse(course);
         cartItem.setCart(cart);
@@ -55,6 +74,6 @@ public class CartController {
         cartItemService.saveCartItem(cartItem);
 
 
-        return "basket";
+        return "redirect:/cart";
     }
 }
